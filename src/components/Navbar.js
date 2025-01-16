73cadbf60aedadc0f105e2b7072b61bfd717bc75
David A. Vargas <davidavargas.dev@gmail.com>
2025-01-16 14:38:03 -0600

    button css added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,18 @@ import { link } from 'react-router-dom'
 
 function Navbar() {
     const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
 
     const handleClick = () => setClick(!click);
     const closeMoblieMenu = () => setClick(false);
+
+    const showButton = () => {
+      if(window.innerWidth <= 960 ) {
+        setButton(false);
+      } else {
+        setButton(true);
+      }
+    };
 
   return (
     <>
@@ -35,6 +44,8 @@ function Navbar() {
                 </link>
               </li>
             </ul>
+            {button && <Button buttonStyle='btn--outline'>SIGN UP
+              </Button>}
         </div>
     </nav>
     </>
